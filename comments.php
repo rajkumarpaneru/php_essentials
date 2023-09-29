3591eb842ba499eb3d6ff5c3f09506b10613e80e
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-09-29 20:15:30 +0545

    adding comments in php code

---
+++ comments.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adding comments</title>
+</head>
+<body>
+    <?php
+        //single-line comments
+        # another syntax for single-line comment
+        /**
+         * Multi-line comments
+         */
+
+    ?>
+    <?php echo "Hello World!"; ?> <br />
+    <?php echo "Hello" . " World!"; ?> <br />
+    <?php echo 4 + 5; ?>
+</body>
+</html>
