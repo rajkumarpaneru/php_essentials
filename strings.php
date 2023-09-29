0ca8c5449dce10a5c153c4e0117f535011ea5dcd
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-09-29 20:31:56 +0545

    php strings

---
+++ strings.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strings</title>
+</head>
+<body>
+    <?php
+        $str1 = "Hello";
+        $str2 = "World";
+        $str3 = $str1 . " " . $str2;
+        echo $str3;
+        echo "<br />";
+    ?>
+    <?php
+        echo "$str3 again!<br/>";
+    ?>
+</body>
+</html>
