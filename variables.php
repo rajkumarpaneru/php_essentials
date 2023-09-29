ba9b73d37bfd7e40380197ae752aa04b9a7f82e6
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-09-29 20:26:07 +0545

    php variables

---
+++ variables.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variables</title>
+</head>
+<body>
+    <?php
+        /**
+         * Variables are dynamically typed in php
+         * i.e. the type of a variable depends on a value it's currently holding
+         * and type of variable can change over the course of the script
+         * variable names are case-sensitive
+         * */ 
+        $var1 = 10;
+        echo $var1;
+        
+        echo "<br />";
+
+        $var1 = "Hello";
+        echo $var1;
+
+        echo "<br />";
+
+        $PI = 3.1415;
+        $greet = "Good Morning";
+    ?>
+</body>
+</html>
